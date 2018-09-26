f3847d081c98924944b7515ca3af6d600e0fa9c5
Min Hee  Park <minheekangg@gmail.com>
2018-09-26 03:32:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif
     list = ""
-    if array.length > 2
+    if array.length >= 2
       list = (array[0..-2].join", ") + ", and " + array[-1]
     end
   end
