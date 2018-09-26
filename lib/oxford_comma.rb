96831ec8bd65d47ec017413f1337296cd43991f7
Min Hee  Park <minheekangg@gmail.com>
2018-09-26 03:33:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
-end
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array[-2] + " and " + array[-1]
+  elsif
+    list = ""
+    if array.length > 2
+      list = (array[0..-2].join", ") + ", and " + array[-1]
+    end
+  end
+    return list
+  end
