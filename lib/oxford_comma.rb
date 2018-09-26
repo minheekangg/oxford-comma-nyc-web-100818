88f3a36861686ca130dc162b681b80b2fb316f73
Min Hee  Park <minheekangg@gmail.com>
2018-09-26 03:16:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   if array.length == 1
     return array.join
   elsif array.length > 3
-    return ((array[0..-2]).join(" , ")).concat((array[-2..-1]).join(", and "))
+    return ((array[0..-2]).join(", ")).concat((array[-2..-1]).join(", and "))
   end
 end
